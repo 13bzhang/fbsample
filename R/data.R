e8f07feefde13b42011da5c0d5425a6f66b213a3
13bzhang <baobao.zhang@yale.edu>
2017-03-18 17:14:09 -0400

    Add helpful datasets for ad targets

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' Education Level
+#'
+#' A dataset that contains the keys for targeting specific levels of education.
+#'
+#' @format A dataset with two variables: \code{key} and \code{description}.
+"fb_edu_level"
+
+#' U.S. Ethnic Affinity
+#'
+#' A dataset that contains the ids for targeting specific ethnic groups in the U.S.
+#'
+#' @format A data frame with six variables including the ethnic affinity \code{id} and
+#' \code{description}, along with metadata from the API Targeting Search results.
+"fb_ethnicity"
+
+#' U.S. Politics
+#'
+#' A data set that contains the ids for targeting specific political ideologies
+#' or behaviors in the U.S.
+#'
+#' @format A dataset with 10 variables including the political
+#' ideologies/behaviors \code{id} and \code{description}, along with metadata
+#' from the API Targeting Search results.
+"fb_politics"
+
+#' U.S. States and Regions
+#'
+#' A data set that contains the keys for targeting states or regions within the U.S.
+#'
+#' @format A dataset with 5 variables including the \code{key} for each of the 50 U.S.
+#' states along with its corresponding Census region.
+"fb_US_states"
+
+
