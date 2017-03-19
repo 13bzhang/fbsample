a61e6f492be233b0798e3485d8999f29aeb57190
13bzhang <baobao.zhang@yale.edu>
2017-03-19 02:29:53 -0400

    fix problem with data file extension

--- R/data.R
+++ R/data.R
@@ -2,12 +2,20 @@
 #'
 #' A dataset that contains the keys for targeting specific levels of education.
 #'
+#' @docType data
+#'
+#' @usage data(fb_edu_level)
+#'
 #' @format A dataset with two variables: \code{key} and \code{description}.
 "fb_edu_level"
 
 #' U.S. Ethnic Affinity
 #'
 #' A dataset that contains the ids for targeting specific ethnic groups in the U.S.
+#'
+#' @docType data
+#'
+#' @usage data(fb_ethnicity)
 #'
 #' @format A data frame with six variables including the ethnic affinity \code{id} and
 #' \code{description}, along with metadata from the API Targeting Search results.
@@ -18,6 +26,10 @@
 #' A data set that contains the ids for targeting specific political ideologies
 #' or behaviors in the U.S.
 #'
+#' @docType data
+#'
+#' @usage data(fb_politics)
+#'
 #' @format A dataset with 10 variables including the political
 #' ideologies/behaviors \code{id} and \code{description}, along with metadata
 #' from the API Targeting Search results.
@@ -27,8 +39,11 @@
 #'
 #' A data set that contains the keys for targeting states or regions within the U.S.
 #'
+#' @docType data
+#'
+#' @usage data(fb_US_states)
+#'
 #' @format A dataset with 5 variables including the \code{key} for each of the 50 U.S.
 #' states along with its corresponding Census region.
 "fb_US_states"
-
 
